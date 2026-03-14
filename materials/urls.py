04aae471c61ff13f27646a948892e2835d238023
Светлана Рогова <mifistofiya@gmail.com>
2026-03-15 02:25:50 +0300

    Add view for Lesson

--- materials/urls.py
+++ materials/urls.py
@@ -1,5 +1,7 @@
-from materials.views import CourseViewSet
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 
 
 app_name = 'materials'
@@ -8,5 +10,9 @@ router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
 
 urlpatterns = [
-
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lesson/retrieve/<int:pk>', LessonRetrieveAPIView.as_view(), name='lesson-retrieve'),
+    path('lesson/update/<int:pk>', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/delete/<int:pk>', LessonDestroyAPIView.as_view(), name='lesson-delete'),
 ] + router.urls
--- materials/views.py
+++ materials/views.py
@@ -9,3 +9,24 @@ class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
 
 
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
