a2b2609e50a39f386216598a4a4a07d76d766d94
Светлана Рогова <mifistofiya@gmail.com>
2026-03-16 21:44:29 +0300

    Add admin

--- materials/models.py
+++ materials/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
-    image = models.ImageField(upload_to='images/', verbose_name='Картинка')
+    image = models.ImageField(upload_to='images/', verbose_name='Картинка', blank=True, null=True)
     description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Course(models.Model):
 
 class Lesson(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
-    image = models.ImageField(upload_to='images/', verbose_name='Картинка')
+    image = models.ImageField(upload_to='images/', verbose_name='Картинка', blank=True, null=True)
     description = models.TextField(verbose_name='Описание')
     video_url = models.URLField(verbose_name="Ссылка на видео")
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import CustomUser, Payments
+
+
+@admin.register(CustomUser)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone_number', 'avatar', 'country')
+    search_fields = ('email',)
+
+@admin.register(Payments)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'payment_date', 'paid_course', 'paid_lesson', 'payment_amount', 'payment_method')
+    search_fields = ('user',)
+
+
