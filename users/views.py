ee7bb965d5a30193ece4a4314606501f95bcae89
Светлана Рогова <mifistofiya@gmail.com>
2026-03-17 20:25:58 +0300

    Add filter ang ordering for Payments

--- users/views.py
+++ users/views.py
@@ -1,10 +1,21 @@
-from rest_framework.generics import RetrieveUpdateAPIView
-from users.models import CustomUser
-from users.serializers import UserSerializer
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
+from users.models import CustomUser, Payments
+from users.serializers import UserSerializer, PaymentsSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 
 class UserEditView(RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+
+class PaymentsList(generics.ListAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['paid_course', 'paid_lesson', 'payment_method']
+    ordering_fields = ['payment_date']
