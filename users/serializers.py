30fdb4acda17aa29f5f1311139ba714eeb99a424
Светлана Рогова <mifistofiya@gmail.com>
2026-03-17 20:25:58 +0300

    Add filter ang ordering for Payments

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from users.models import CustomUser
+from users.models import CustomUser, Payments
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
+        fields = '__all__'
+
+
+class PaymentsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payments
         fields = '__all__'
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from users.models import CustomUser
-from users.views import UserEditView
+from users.views import UserEditView, PaymentsList
 
 app_name = 'users'
 
 urlpatterns = [
-    path('users/', UserEditView.as_view(queryset=CustomUser.objects.all(), serializer_class='UserSerializer'), name='user-list')
+    path('users/', UserEditView.as_view(queryset=CustomUser.objects.all(), serializer_class='UserSerializer'), name='user-list'),
+    path('payments/', PaymentsList.as_view(), name='payments-list'),
 ]
 
