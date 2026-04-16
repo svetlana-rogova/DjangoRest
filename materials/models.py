0534bdcf27bda360e8379f124c5ac6f27cc8f590
Светлана Рогова <mifistofiya@gmail.com>
2026-04-16 22:12:14 +0300

    Add owner for course and lesson

--- materials/models.py
+++ materials/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
+
 class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
     image = models.ImageField(upload_to='images/', verbose_name='Картинка', blank=True, null=True)
     description = models.TextField(verbose_name='Описание')
+    owner = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, related_name='course', null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
@@ -20,6 +23,7 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='Описание')
     video_url = models.URLField(verbose_name="Ссылка на видео")
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
+    owner = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, related_name='lesson', null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
