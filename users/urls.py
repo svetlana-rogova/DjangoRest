a22513cb6fa3ee56db730b3a253308f6ae3c6cfb
Светлана Рогова <mifistofiya@gmail.com>
2026-04-17 23:21:08 +0300

    Fix usls for users

--- users/urls.py
+++ users/urls.py
@@ -6,14 +6,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 app_name = 'users'
 
 
-
 urlpatterns = [
-    path('users/me/', UserEditView.as_view(), name='user-list'),
-    path('payments/', PaymentsList.as_view(), name='payments-list'),
+    path('users/me/', UserEditView.as_view(), name='user-me'),
+    path('payments/', PaymentsList.as_view(), name='payments_list'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/', UserListView.as_view()),
-    path('users/<int:pk>/', UserDetailView.as_view()),
-    path('users/delete/', UserDeleteView.as_view()),
-    path('register/', RegisterView.as_view()),
+    path('users/', UserListView.as_view(), name='users_list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='users_detail'),
+    path('users/delete/', UserDeleteView.as_view(), name='users_delete'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
