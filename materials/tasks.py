22a52e3dbf3a52d51b2cc5b00a93b763a711f0b5
Светлана Рогова <mifistofiya@gmail.com>
2026-05-01 15:05:16 +0300

    Fix tasks for send_mail_user

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,15 +1,25 @@
-from users.models import CustomUser
-from materials.models import Subscription
+from django.utils import timezone
+from datetime import timedelta
+from materials.models import Subscription, Course
 from celery import shared_task
 from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 @shared_task
 def send_mail_user(course_id):
+    """
+    Задача на рассылку сообщений подписчикам при обновлении курса при условии, что курс не обновлялся последние 4 часа
+    """
     subscription = Subscription.objects.filter(course_id=course_id)
-    for sub in subscription:
-        subject = 'Обновление курса'
-        message = 'Привет. В твой курс добавили новую информацию. Скорее посмотри что изменилось.'
-        from_email = 'mifistofiya@gmail.com'
-        recipient_list = [sub.owner.email]
-        send_mail(subject, message, from_email, recipient_list)
+    course = get_object_or_404(Course, id=course_id)
+    subject = 'Обновление курса'
+    message = 'Привет. В твой курс добавили новую информацию. Скорее посмотри что изменилось.'
+    from_email = os.getenv('MY_EMAIL')
+    if timezone.now() - course.updated_at > timedelta(hours=4):
+        for sub in subscription:
+            recipient_list = [sub.owner.email]
+            send_mail(subject, message, from_email, recipient_list)
